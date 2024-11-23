f12bcbbc264ee4af14cbb390e704f021062e732a
onekill0503 <gamemasteraji123@gmail.com>
2024-11-23 13:41:12 +0700

    lz

--- src/services/raffle.ts
+++ src/services/raffle.ts
@@ -1,12 +1,10 @@
-import { Wallet } from "ethers";
 import { Raffle } from "../schema/types/Raffle";
 import { UserTicket } from "../schema/types/UserTicket";
-import { getFortuSmartContract, getLatestBlock, getOwnerWallet } from "../utils/blockchain";
+import { getFortuSmartContract, getLatestBlock } from "../utils/blockchain";
 import { getRaffleData } from "../utils/subgraph";
 
 const raffleData = async () => {
     const FortuSmartContract = await getFortuSmartContract();
-    const OperatorWallet: Wallet = await getOwnerWallet();
     const activeBatch = (await FortuSmartContract.currentBatch());
 
     console.log(`[${new Date()}] FORTU : Get Random Number for Batch ${activeBatch} !`)
@@ -16,11 +14,6 @@ const raffleData = async () => {
         return;
     }
     console.log(`[${new Date()}] FORTU : Lucky Number Generated : ${BigInt(getGeneratedLuckyNumber[1]).toString()} !`);
-    // const isOperatorAlreadySubmit: boolean = (await FortuSmartContract.operatorConfirm(activeBatch, OperatorWallet.address))
-    // if(isOperatorAlreadySubmit){
-    //     console.log(`[${new Date()}] FORTU : Operator already submit winner`);
-    //     return;
-    // }
     
     console.log(`[${new Date()}] FORTU : Get Final Raffle Data for Batch ${activeBatch} !`);
     const finalUserRaffleData = await getFinalRaffleData(BigInt(activeBatch));
